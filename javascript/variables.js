4858487bcf3a96faafd1692bee687d0c28adb639
aastharaut <aastharaut03@gmail.com>
2025-11-30 09:29:40 +0545

    folders sorting

---
+++ javascript/variables.js
@@ -0,0 +1,9 @@
+address = "putalisadak"
+phone = 9866298964
+reciever = "aastha raut"
+premium_user = true
+
+console.log("address: ", address);
+console.log("reciever: ", reciever);
+console.log("premium_user: ", premium_user);
+console.log("phone: ", phone);
---
+++ website/variables.js
@@ -0,0 +1,6 @@
+// variables are something that contains value. simply a keyword that can store some values.
+// Data type 
+// text = string, 
+// number =  int
+// float = 1.2 
+// boolean = true/false
